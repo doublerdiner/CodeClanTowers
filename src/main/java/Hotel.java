1d90efb37b6209a131101e3488bb2f82a9c8e095
Chris Barclay <chris.barclay1@gmail.com>
2023-04-13 13:53:26 +0100

    MVP Completed

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    private String name;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel(){
+        this.name = "CodeClan Towers";
+        this.bedrooms = new ArrayList<>();
+        this.conferenceRooms = new ArrayList<>();
+        bedrooms.add(new Bedroom(1, RoomType.SINGLE));
+        bedrooms.add(new Bedroom(2, RoomType.DOUBLE));
+        bedrooms.add(new Bedroom(3, RoomType.TWIN));
+        bedrooms.add(new Bedroom(4, RoomType.HONEYMOON));
+        conferenceRooms.add(new ConferenceRoom(15, "Murray Room"));
+        conferenceRooms.add(new ConferenceRoom(30, "Jute Suite"));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Bedroom> getBedrooms() {
+        return bedrooms;
+    }
+
+    public ArrayList<ConferenceRoom> getConferenceRooms() {
+        return conferenceRooms;
+    }
+
+    public void addGuestToRoom(Room room, Guest guest){
+        room.addGuest(guest);
+    }
+
+    public void removeGuestFromRoom(Room room, Guest guest){
+        room.removeGuest(guest);
+    }
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,53 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+
+    private Hotel hotel;
+    private Guest guest;
+    private Guest guest2;
+    private Bedroom bedroom;
+    private ConferenceRoom conferenceRoom;
+
+    @Before
+    public void setup(){
+        hotel = new Hotel();
+        guest = new Guest("Laura");
+        guest2 = new Guest("Dale");
+    }
+
+    @Test
+    public void hotelHasName(){
+        assertEquals("CodeClan Towers", hotel.getName());
+    }
+
+    @Test
+    public void hotelCanAddGuestToBedroom(){
+        hotel.addGuestToRoom(hotel.getBedrooms().get(3), guest);
+        hotel.addGuestToRoom(hotel.getBedrooms().get(3), guest2);
+        assertEquals(2, hotel.getBedrooms().get(3).getGuests().size());
+    }
+    @Test
+    public void hotelCanRemoveGuestFromBedroom(){
+        hotel.addGuestToRoom(hotel.getBedrooms().get(3), guest);
+        hotel.addGuestToRoom(hotel.getBedrooms().get(3), guest2);
+        hotel.removeGuestFromRoom(hotel.getBedrooms().get(3), guest);
+        assertEquals(1, hotel.getBedrooms().get(3).getGuests().size());
+    }
+    @Test
+    public void hotelCanAddGuestToConferenceRoom(){
+        hotel.addGuestToRoom(hotel.getConferenceRooms().get(0), guest);
+        hotel.addGuestToRoom(hotel.getConferenceRooms().get(0), guest2);
+        assertEquals(2, hotel.getConferenceRooms().get(0).getGuests().size());
+    }
+    @Test
+    public void hotelCanRemoveGuestFromConferenceRoom(){
+        hotel.addGuestToRoom(hotel.getConferenceRooms().get(0), guest);
+        hotel.addGuestToRoom(hotel.getConferenceRooms().get(0), guest2);
+        hotel.removeGuestFromRoom(hotel.getConferenceRooms().get(0), guest);
+        assertEquals(1, hotel.getConferenceRooms().get(0).getGuests().size());
+    }
+
 }
