64b4ac3032d8368ab0e5529aafbcb87766856032
Chris Barclay <chris.barclay1@gmail.com>
2023-04-13 14:02:23 +0100

    Booking created and tested

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,2 +1,18 @@
 public class Booking {
+
+    private Bedroom bedroom;
+    private int numberOfNightsBooked;
+
+    public Booking(Bedroom bedroom, int numberOfNightsBooked) {
+        this.bedroom = bedroom;
+        this.numberOfNightsBooked = numberOfNightsBooked;
+    }
+
+    public Bedroom getBedroom() {
+        return bedroom;
+    }
+
+    public int getNumberOfNightsBooked() {
+        return numberOfNightsBooked;
+    }
 }
---
+++ src/test/java/BookingTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookingTest {
+
+    private Booking booking;
+    private Hotel hotel;
+
+    @Before
+    public void setup(){
+        hotel = new Hotel();
+        booking = new Booking(hotel.getBedrooms().get(0),5);
+    }
+
+    @Test
+    public void bookingHasRoom(){
+        assertEquals("Single", booking.getBedroom().getRoomType().getFormattedName());
+        assertEquals(1, booking.getBedroom().getRoomNumber());
+    }
+    @Test
+    public void bookingHasNumberOfNights(){
+        assertEquals(5, booking.getNumberOfNightsBooked());
+    }
+
+
+
+
+}
